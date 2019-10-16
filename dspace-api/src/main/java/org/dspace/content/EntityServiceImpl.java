atmire/DSpace

DS-4096 paginated methods for relationships

dspace-api/src/main/java/org/dspace/content/EntityServiceImpl.java
@@ -37,8 +37,15 @@ public class EntityServiceImpl implements EntityService {
 
     @Override
     public Entity findByItemId(Context context, UUID itemId) throws SQLException {
+
+        return findByItemId(context, itemId, -1, -1);
+    }
+
+    @Override
+    public Entity findByItemId(Context context, UUID itemId, Integer limit, Integer offset) throws SQLException {
+
         Item item = itemService.find(context, itemId);
-        List<Relationship> relationshipList = relationshipService.findByItem(context, item);
+        List<Relationship> relationshipList = relationshipService.findByItem(context, item, limit, offset);
         return new Entity(item, relationshipList);
     }
 
@@ -79,8 +86,16 @@ public class EntityServiceImpl implements EntityService {
 
     @Override
     public List<Relationship> getRelationsByLabel(Context context, String label) throws SQLException {
+
+        return getRelationsByLabel(context, label, -1, -1);
+    }
+
+    @Override
+    public List<Relationship> getRelationsByLabel(Context context, String label, Integer limit, Integer offset)
+            throws SQLException {
+
         List<Relationship> listToReturn = new LinkedList<>();
-        List<Relationship> relationshipList = relationshipService.findAll(context);
+        List<Relationship> relationshipList = relationshipService.findAll(context, limit, offset);
         for (Relationship relationship : relationshipList) {
             RelationshipType relationshipType = relationship.getRelationshipType();
             if (StringUtils.equals(relationshipType.getLeftLabel(),label) ||
@@ -93,9 +108,17 @@ public class EntityServiceImpl implements EntityService {
 
     @Override
     public List<RelationshipType> getAllRelationshipTypes(Context context, Entity entity) throws SQLException {
+
+        return getAllRelationshipTypes(context, entity, -1, -1);
+    }
+
+    @Override
+    public List<RelationshipType> getAllRelationshipTypes(Context context, Entity entity, Integer limit, Integer offset)
+            throws SQLException {
+
         EntityType entityType = this.getType(context, entity);
         List<RelationshipType> listToReturn = new LinkedList<>();
-        for (RelationshipType relationshipType : relationshipTypeService.findAll(context)) {
+        for (RelationshipType relationshipType : relationshipTypeService.findAll(context, limit, offset)) {
             if (relationshipType.getLeftType().getID() == entityType.getID() ||
                 relationshipType.getRightType().getID() == entityType.getID()) {
                 listToReturn.add(relationshipType);
@@ -106,9 +129,17 @@ public class EntityServiceImpl implements EntityService {
 
     @Override
     public List<RelationshipType> getLeftRelationshipTypes(Context context, Entity entity) throws SQLException {
+
+        return getLeftRelationshipTypes(context, entity, -1, -1);
+    }
+
+    @Override
+    public List<RelationshipType> getLeftRelationshipTypes(Context context, Entity entity,
+                                                           Integer limit, Integer offset) throws SQLException {
+
         EntityType entityType = this.getType(context, entity);
         List<RelationshipType> listToReturn = new LinkedList<>();
-        for (RelationshipType relationshipType : relationshipTypeService.findAll(context)) {
+        for (RelationshipType relationshipType : relationshipTypeService.findAll(context, limit, offset)) {
             if (relationshipType.getLeftType().getID() == entityType.getID()) {
                 listToReturn.add(relationshipType);
             }
@@ -118,9 +149,17 @@ public class EntityServiceImpl implements EntityService {
 
     @Override
     public List<RelationshipType> getRightRelationshipTypes(Context context, Entity entity) throws SQLException {
+
+        return getRightRelationshipTypes(context, entity, -1, -1);
+    }
+
+    @Override
+    public List<RelationshipType> getRightRelationshipTypes(Context context, Entity entity,
+                                                            Integer limit, Integer offset) throws SQLException {
+
         EntityType entityType = this.getType(context, entity);
         List<RelationshipType> listToReturn = new LinkedList<>();
-        for (RelationshipType relationshipType : relationshipTypeService.findAll(context)) {
+        for (RelationshipType relationshipType : relationshipTypeService.findAll(context, limit, offset)) {
             if (relationshipType.getRightType().getID() == entityType.getID()) {
                 listToReturn.add(relationshipType);
             }
@@ -130,8 +169,16 @@ public class EntityServiceImpl implements EntityService {
 
     @Override
     public List<RelationshipType> getRelationshipTypesByLabel(Context context, String label) throws SQLException {
+
+        return getRelationshipTypesByLabel(context, label, -1, -1);
+    }
+
+    @Override
+    public List<RelationshipType> getRelationshipTypesByLabel(Context context, String label,
+                                                              Integer limit, Integer offset) throws SQLException {
+
         List<RelationshipType> listToReturn = new LinkedList<>();
-        for (RelationshipType relationshipType : relationshipTypeService.findAll(context)) {
+        for (RelationshipType relationshipType : relationshipTypeService.findAll(context, limit, offset)) {
             if (StringUtils.equals(relationshipType.getLeftLabel(),label) ||
                 StringUtils.equals(relationshipType.getRightLabel(),label)) {
                 listToReturn.add(relationshipType);

dspace-api/src/main/java/org/dspace/content/RelationshipTypeServiceImpl.java
@@ -54,17 +54,39 @@ public class RelationshipTypeServiceImpl implements RelationshipTypeService {
 
     @Override
     public List<RelationshipType> findAll(Context context) throws SQLException {
-        return relationshipTypeDAO.findAll(context, RelationshipType.class);
+
+        return findAll(context, -1, -1);
+    }
+
+    @Override
+    public List<RelationshipType> findAll(Context context, Integer limit, Integer offset) throws SQLException {
+
+        return relationshipTypeDAO.findAll(context, RelationshipType.class, limit, offset);
     }
 
     @Override
     public List<RelationshipType> findByLeftOrRightLabel(Context context, String label) throws SQLException {
-        return relationshipTypeDAO.findByLeftOrRightLabel(context, label);
+
+        return findByLeftOrRightLabel(context, label, -1, -1);
+    }
+
+    @Override
+    public List<RelationshipType> findByLeftOrRightLabel(Context context, String label, Integer limit, Integer offset)
+            throws SQLException {
+
+        return relationshipTypeDAO.findByLeftOrRightLabel(context, label, limit, offset);
     }
 
     @Override
     public List<RelationshipType> findByEntityType(Context context, EntityType entityType) throws SQLException {
-        return relationshipTypeDAO.findByEntityType(context, entityType);
+        return findByEntityType(context, entityType, -1, -1);
+    }
+
+    @Override
+    public List<RelationshipType> findByEntityType(Context context, EntityType entityType,
+                                                   Integer limit, Integer offset) throws SQLException {
+
+        return relationshipTypeDAO.findByEntityType(context, entityType, limit, offset);
     }
 
     @Override

dspace-api/src/main/java/org/dspace/content/dao/RelationshipTypeDAO.java
@@ -49,6 +49,19 @@ public interface RelationshipTypeDAO extends GenericDAO<RelationshipType> {
     List<RelationshipType> findByLeftOrRightLabel(Context context, String label) throws SQLException;
 
     /**
+     * This method will return a list of RelationshipType objects for which the given label is equal to
+     * either the leftLabel or rightLabel.
+     * @param context   The relevant DSpace context
+     * @param label     The label that will be used to check on
+     * @param limit     paging limit
+     * @param offset    paging offset
+     * @return A list of RelationshipType objects that have the given label as either the leftLabel or rightLabel
+     * @throws SQLException If something goes wrong
+     */
+    List<RelationshipType> findByLeftOrRightLabel(Context context, String label, Integer limit, Integer offset)
+            throws SQLException;
+
+    /**
      * This method will return a list of RelationshipType objects for which the given EntityType object is equal
      * to the leftType or rightType
      * @param context       The relevant DSpace context
@@ -58,4 +71,18 @@ public interface RelationshipTypeDAO extends GenericDAO<RelationshipType> {
      * @throws SQLException If something goes wrong
      */
     List<RelationshipType> findByEntityType(Context context, EntityType entityType) throws SQLException;
+
+    /**
+     * This method will return a list of RelationshipType objects for which the given EntityType object is equal
+     * to the leftType or rightType
+     * @param context       The relevant DSpace context
+     * @param entityType    The EntityType object that will be used to check on
+     * @param limit         paging limit
+     * @param offset        paging offset
+     * @return  The list of RelationshipType objects that have the given EntityType object
+     *          as either a leftType or rightType
+     * @throws SQLException If something goes wrong
+     */
+    List<RelationshipType> findByEntityType(Context context, EntityType entityType, Integer limit, Integer offset)
+            throws SQLException;
 }

dspace-api/src/main/java/org/dspace/content/dao/impl/RelationshipTypeDAOImpl.java
@@ -41,6 +41,14 @@ public class RelationshipTypeDAOImpl extends AbstractHibernateDAO<RelationshipTy
 
     @Override
     public List<RelationshipType> findByLeftOrRightLabel(Context context, String label) throws SQLException {
+
+        return findByLeftOrRightLabel(context, label, -1, -1);
+    }
+
+    @Override
+    public List<RelationshipType> findByLeftOrRightLabel(Context context, String label, Integer limit, Integer offset)
+            throws SQLException {
+
         CriteriaBuilder criteriaBuilder = getCriteriaBuilder(context);
         CriteriaQuery criteriaQuery = getCriteriaQuery(criteriaBuilder, RelationshipType.class);
         Root<RelationshipType> relationshipTypeRoot = criteriaQuery.from(RelationshipType.class);
@@ -51,11 +59,18 @@ public class RelationshipTypeDAOImpl extends AbstractHibernateDAO<RelationshipTy
                 criteriaBuilder.equal(relationshipTypeRoot.get(RelationshipType_.rightLabel), label)
             )
         );
-        return list(context, criteriaQuery, true, RelationshipType.class, -1, -1);
+        return list(context, criteriaQuery, true, RelationshipType.class, limit, offset);
     }
 
     @Override
     public List<RelationshipType> findByEntityType(Context context, EntityType entityType) throws SQLException {
+        return findByEntityType(context, entityType, -1, -1);
+    }
+
+    @Override
+    public List<RelationshipType> findByEntityType(Context context, EntityType entityType,
+                                                   Integer limit, Integer offset) throws SQLException {
+
         CriteriaBuilder criteriaBuilder = getCriteriaBuilder(context);
         CriteriaQuery criteriaQuery = getCriteriaQuery(criteriaBuilder, RelationshipType.class);
         Root<RelationshipType> relationshipTypeRoot = criteriaQuery.from(RelationshipType.class);
@@ -67,7 +82,7 @@ public class RelationshipTypeDAOImpl extends AbstractHibernateDAO<RelationshipTy
                                    .equal(relationshipTypeRoot.get(RelationshipType_.rightType), entityType)
             )
         );
-        return list(context, criteriaQuery, false, RelationshipType.class, -1, -1);
+        return list(context, criteriaQuery, false, RelationshipType.class, limit, offset);
     }
 
 }

juliapim/DSpace-1

dspace-api/src/main/java/org/dspace/content/EntityServiceImpl.java
@@ -38,8 +38,15 @@ public class EntityServiceImpl implements EntityService {
 
     @Override
     public Entity findByItemId(Context context, UUID itemId) throws SQLException {
+
+        return findByItemId(context, itemId, -1, -1);
+    }
+
+    @Override
+    public Entity findByItemId(Context context, UUID itemId, Integer limit, Integer offset) throws SQLException {
+
         Item item = itemService.find(context, itemId);
-        List<Relationship> relationshipList = relationshipService.findByItem(context, item);
+        List<Relationship> relationshipList = relationshipService.findByItem(context, item, limit, offset);
         return new Entity(item, relationshipList);
     }
 
@@ -80,8 +87,16 @@ public class EntityServiceImpl implements EntityService {
 
     @Override
     public List<Relationship> getRelationsByLabel(Context context, String label) throws SQLException {
+
+        return getRelationsByLabel(context, label, -1, -1);
+    }
+
+    @Override
+    public List<Relationship> getRelationsByLabel(Context context, String label, Integer limit, Integer offset)
+            throws SQLException {
+
         List<Relationship> listToReturn = new LinkedList<>();
-        List<Relationship> relationshipList = relationshipService.findAll(context);
+        List<Relationship> relationshipList = relationshipService.findAll(context, limit, offset);
         for (Relationship relationship : relationshipList) {
             RelationshipType relationshipType = relationship.getRelationshipType();
             if (StringUtils.equals(relationshipType.getLeftwardType(),label) ||
@@ -94,12 +109,20 @@ public class EntityServiceImpl implements EntityService {
 
     @Override
     public List<RelationshipType> getAllRelationshipTypes(Context context, Entity entity) throws SQLException {
+
+        return getAllRelationshipTypes(context, entity, -1, -1);
+    }
+
+    @Override
+    public List<RelationshipType> getAllRelationshipTypes(Context context, Entity entity, Integer limit, Integer offset)
+            throws SQLException {
+
         EntityType entityType = this.getType(context, entity);
         if (entityType == null) {
             return Collections.emptyList();
         }
         List<RelationshipType> listToReturn = new LinkedList<>();
-        for (RelationshipType relationshipType : relationshipTypeService.findAll(context)) {
+        for (RelationshipType relationshipType : relationshipTypeService.findAll(context, limit, offset)) {
             if (relationshipType.getLeftType().getID() == entityType.getID() ||
                 relationshipType.getRightType().getID() == entityType.getID()) {
                 listToReturn.add(relationshipType);
@@ -110,9 +133,17 @@ public class EntityServiceImpl implements EntityService {
 
     @Override
     public List<RelationshipType> getLeftRelationshipTypes(Context context, Entity entity) throws SQLException {
+
+        return getLeftRelationshipTypes(context, entity, -1, -1);
+    }
+
+    @Override
+    public List<RelationshipType> getLeftRelationshipTypes(Context context, Entity entity,
+                                                           Integer limit, Integer offset) throws SQLException {
+
         EntityType entityType = this.getType(context, entity);
         List<RelationshipType> listToReturn = new LinkedList<>();
-        for (RelationshipType relationshipType : relationshipTypeService.findAll(context)) {
+        for (RelationshipType relationshipType : relationshipTypeService.findAll(context, limit, offset)) {
             if (relationshipType.getLeftType().getID() == entityType.getID()) {
                 listToReturn.add(relationshipType);
             }
@@ -122,9 +153,17 @@ public class EntityServiceImpl implements EntityService {
 
     @Override
     public List<RelationshipType> getRightRelationshipTypes(Context context, Entity entity) throws SQLException {
+
+        return getRightRelationshipTypes(context, entity, -1, -1);
+    }
+
+    @Override
+    public List<RelationshipType> getRightRelationshipTypes(Context context, Entity entity,
+                                                            Integer limit, Integer offset) throws SQLException {
+
         EntityType entityType = this.getType(context, entity);
         List<RelationshipType> listToReturn = new LinkedList<>();
-        for (RelationshipType relationshipType : relationshipTypeService.findAll(context)) {
+        for (RelationshipType relationshipType : relationshipTypeService.findAll(context, limit, offset)) {
             if (relationshipType.getRightType().getID() == entityType.getID()) {
                 listToReturn.add(relationshipType);
             }
@@ -133,9 +172,16 @@ public class EntityServiceImpl implements EntityService {
     }
 
     @Override
-    public List<RelationshipType> getRelationshipTypesByTypeName(Context context, String label) throws SQLException {
+    public List<RelationshipType> getRelationshipTypesByLabel(Context context, String label) throws SQLException {
+
+        return getRelationshipTypesByLabel(context, label, -1, -1);
+    }
+
+    @Override
+    public List<RelationshipType> getRelationshipTypesByTypeName(Context context, String label,
+                                                                 Integer limit, Integer offset) throws SQLException {
         List<RelationshipType> listToReturn = new LinkedList<>();
-        for (RelationshipType relationshipType : relationshipTypeService.findAll(context)) {
+        for (RelationshipType relationshipType : relationshipTypeService.findAll(context, limit, offset)) {
             if (StringUtils.equals(relationshipType.getLeftwardType(),label) ||
                 StringUtils.equals(relationshipType.getRightwardType(),label)) {
                 listToReturn.add(relationshipType);

dspace-api/src/main/java/org/dspace/content/RelationshipTypeServiceImpl.java
@@ -54,17 +54,35 @@ public class RelationshipTypeServiceImpl implements RelationshipTypeService {
 
     @Override
     public List<RelationshipType> findAll(Context context) throws SQLException {
-        return relationshipTypeDAO.findAll(context, RelationshipType.class);
+        return findAll(context, -1, -1);
+    }
+
+    @Override
+    public List<RelationshipType> findAll(Context context, Integer limit, Integer offset) throws SQLException {
+
+        return relationshipTypeDAO.findAll(context, RelationshipType.class, limit, offset);
     }
 
     @Override
     public List<RelationshipType> findByLeftwardOrRightwardTypeName(Context context, String label) throws SQLException {
-        return relationshipTypeDAO.findByLeftwardOrRightwardTypeName(context, label);
+        return findByLeftOrRightLabel(context, label, -1, -1);
+    }
+
+    @Override
+    public List<RelationshipType> findByLeftwardOrRightwardTypeName(Context context, String label, Integer limit, Integer offset)
+            throws SQLException {
+        return relationshipTypeDAO.findByLeftOrRightLabel(context, label, limit, offset);
     }
 
     @Override
     public List<RelationshipType> findByEntityType(Context context, EntityType entityType) throws SQLException {
-        return relationshipTypeDAO.findByEntityType(context, entityType);
+        return findByEntityType(context, entityType, -1, -1);
+    }
+
+    @Override
+    public List<RelationshipType> findByEntityType(Context context, EntityType entityType,
+                                                   Integer limit, Integer offset) throws SQLException {
+        return relationshipTypeDAO.findByEntityType(context, entityType, limit, offset);
     }
 
     @Override

dspace-api/src/main/java/org/dspace/content/dao/RelationshipTypeDAO.java
@@ -51,6 +51,19 @@ public interface RelationshipTypeDAO extends GenericDAO<RelationshipType> {
     List<RelationshipType> findByLeftwardOrRightwardTypeName(Context context, String type) throws SQLException;
 
     /**
+     * This method will return a list of RelationshipType objects for which the given label is equal to
+     * either the leftLabel or rightLabel.
+     * @param context   The relevant DSpace context
+     * @param label     The label that will be used to check on
+     * @param limit     paging limit
+     * @param offset    paging offset
+     * @return A list of RelationshipType objects that have the given label as either the leftLabel or rightLabel
+     * @throws SQLException If something goes wrong
+     */
+    List<RelationshipType> findByLeftwardOrRightwardTypeName(Context context, String label, Integer limit, Integer offset)
+            throws SQLException;
+
+    /**
      * This method will return a list of RelationshipType objects for which the given EntityType object is equal
      * to the leftType or rightType
      * @param context       The relevant DSpace context
@@ -60,4 +73,18 @@ public interface RelationshipTypeDAO extends GenericDAO<RelationshipType> {
      * @throws SQLException If something goes wrong
      */
     List<RelationshipType> findByEntityType(Context context, EntityType entityType) throws SQLException;
+
+    /**
+     * This method will return a list of RelationshipType objects for which the given EntityType object is equal
+     * to the leftType or rightType
+     * @param context       The relevant DSpace context
+     * @param entityType    The EntityType object that will be used to check on
+     * @param limit         paging limit
+     * @param offset        paging offset
+     * @return  The list of RelationshipType objects that have the given EntityType object
+     *          as either a leftType or rightType
+     * @throws SQLException If something goes wrong
+     */
+    List<RelationshipType> findByEntityType(Context context, EntityType entityType, Integer limit, Integer offset)
+            throws SQLException;
 }

dspace-api/src/main/java/org/dspace/content/dao/impl/RelationshipTypeDAOImpl.java
@@ -41,6 +41,14 @@ public class RelationshipTypeDAOImpl extends AbstractHibernateDAO<RelationshipTy
 
     @Override
     public List<RelationshipType> findByLeftwardOrRightwardTypeName(Context context, String type) throws SQLException {
+
+        return findByLeftwardOrRightwardTypeName(context, type, -1, -1);
+    }
+
+    @Override
+    public List<RelationshipType> findByLeftwardOrRightwardTypeName(Context context, String type, Integer limit, Integer offset)
+            throws SQLException {
+
         CriteriaBuilder criteriaBuilder = getCriteriaBuilder(context);
         CriteriaQuery criteriaQuery = getCriteriaQuery(criteriaBuilder, RelationshipType.class);
         Root<RelationshipType> relationshipTypeRoot = criteriaQuery.from(RelationshipType.class);
@@ -51,11 +59,18 @@ public class RelationshipTypeDAOImpl extends AbstractHibernateDAO<RelationshipTy
                 criteriaBuilder.equal(relationshipTypeRoot.get(RelationshipType_.rightwardType), type)
             )
         );
-        return list(context, criteriaQuery, true, RelationshipType.class, -1, -1);
+        return list(context, criteriaQuery, true, RelationshipType.class, limit, offset);
     }
 
     @Override
     public List<RelationshipType> findByEntityType(Context context, EntityType entityType) throws SQLException {
+        return findByEntityType(context, entityType, -1, -1);
+    }
+
+    @Override
+    public List<RelationshipType> findByEntityType(Context context, EntityType entityType,
+                                                   Integer limit, Integer offset) throws SQLException {
+
         CriteriaBuilder criteriaBuilder = getCriteriaBuilder(context);
         CriteriaQuery criteriaQuery = getCriteriaQuery(criteriaBuilder, RelationshipType.class);
         Root<RelationshipType> relationshipTypeRoot = criteriaQuery.from(RelationshipType.class);
@@ -67,7 +82,7 @@ public class RelationshipTypeDAOImpl extends AbstractHibernateDAO<RelationshipTy
                                    .equal(relationshipTypeRoot.get(RelationshipType_.rightType), entityType)
             )
         );
-        return list(context, criteriaQuery, false, RelationshipType.class, -1, -1);
+        return list(context, criteriaQuery, false, RelationshipType.class, limit, offset);
     }
 
 }